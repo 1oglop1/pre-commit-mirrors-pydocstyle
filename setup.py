bb0d60c6e4e20a2c0e0e638db76bbd46fd203ec1
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2017-08-21 13:14:09 +0200

    updated pydocstyle version

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre-commit-mirrors-pydocstyle',
     version='0.0.0',
-    install_requires=['pydocstyle==1.0.0'],
+    install_requires=['pydocstyle==2.0.0'],
 )
